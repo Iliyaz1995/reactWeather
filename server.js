8d862da46b2809cd0343c917b0863f3f2bf66e55
iliyazUI <iliyazUI@github.com>
2018-01-18 22:18:12 +0530

    bug

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-const express = require('express');
+var express = require('express');
 
-const PORT = process.env.PORT || 3000
+// Create our app
 var app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use(function(req, res, next){
+app.use(function (req, res, next){
   if (req.headers['x-forwarded-proto'] === 'http') {
     next();
-  }else {
+  } else {
     res.redirect('http://' + req.hostname + req.url);
   }
 });
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.listen(PORT, function() {
-  console.log('server is up at '+ PORT);
-})
+app.listen(PORT, function () {
+  console.log('Express server is up on port ' + PORT);
+});
